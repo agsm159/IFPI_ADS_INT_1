ccc2fd58c572055564d54cfd7fd951610a92b7d4
Antônio Gabriel <61834224+agsm159@users.noreply.github.com>
2021-06-18 15:26:07 -0300

    Update Q01.ts

--- Atividade 10-06-2021/Q01.ts
+++ Atividade 10-06-2021/Q01.ts
@@ -13,36 +13,74 @@ interface Iteravel<T>{
 class ColecaoArray<T>{
     
     private _colecao: T[] = [];
-    
+    cont: number = 0;
+
     adiciona(elemento: T){
+
         this._colecao.push(elemento);
     }
 
     proximo(){
-        return this._colecao.pop();
+
+        return this._colecao[this.cont++];
     }
 
     atual(){
-        return this._colecao;
+
+        console.log('Atual: ')
+
+        return this._colecao[this.cont];
     }
 
     isPrimeiro(){ 
-        return this._colecao[0];
+
+        console.log('É o primeiro valor? ')
+
+        if(this._colecao[this.cont] == this._colecao[0]){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     isUltimo(){
-        
+
+        console.log('É o último valor? ')
+        if(this._colecao[this.cont] == this._colecao[this._colecao.length -1] ){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     isParaPrimeiro(){
 
+        this.cont = 0
+        console.log('\nO iterador retornou ao primeiro valor\n');
+        return (this._colecao[this.cont])
     }
 }
 
 let p = new ColecaoArray<number>();
 
-p.adiciona(0);
 p.adiciona(1);
+p.adiciona(2);
+p.adiciona(3);
+
 console.log(p.atual());
-console.log(p.proximo());
 console.log(p.isPrimeiro());
+p.proximo()
+console.log(p.atual());
+console.log(p.isPrimeiro());
+p.proximo()
+console.log(p.atual());
+console.log(p.isUltimo());
+p.isParaPrimeiro()
+console.log(p.atual());
+p.proximo()
+console.log(p.atual());
+p.proximo()
+console.log(p.atual());
+console.log(p.isUltimo());
