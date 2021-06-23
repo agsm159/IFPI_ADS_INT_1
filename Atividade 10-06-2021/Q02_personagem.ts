9feffdddeb1b45e3d9a959babbac0f7d06ea09df
Antônio Gabriel <61834224+agsm159@users.noreply.github.com>
2021-06-22 23:01:21 -0300

    Update Q02_personagem.ts

--- Atividade 10-06-2021/Q02_personagem.ts
+++ Atividade 10-06-2021/Q02_personagem.ts
@@ -9,7 +9,7 @@ export class Personagem{
         this.nome = nome
         this.energia = energia
     }
-    estarVivo(){
+    estaVivo(){
         if(this.energia > 0){
             return true
         }
@@ -68,11 +68,3 @@ export class Cavaleiro extends Soldado{
         }
     }
 }
-
-let personagem = new Personagem(1, 'artorias', 100)
-let soldado1 = new Soldado(2, 'Siff', 100, 15)
-let soldado2 = new Soldado(3, 'Ornstein', 100, 18)
-let caveleiro = new Cavaleiro(4, 'Doko', 100, 30)
-
-soldado1.atacar(soldado2)
-console.log(soldado2.energia)
